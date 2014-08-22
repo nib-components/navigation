67117b7b449794163c80f6ff4a27155c19140aef
Nick Ellercamp <n.ellercamp@nib.com.au>
2014-08-22 14:50:25 +1000

    initial commit

--- README.md
+++ README.md
@@ -1,2 +1,21 @@
 navigation
 ==========
+
+Navigation component for navigating around the places.
+
+Markup
+======
+
+    <div class="quote-nav js-navigation">
+        <ul class="nav nav--horizontal">
+            <li class="nav-item"><a class="nav-item__link js-nav-item" href="/some/place/else" data-page="page-name-1">OMG Page 1</a></li>
+            <li class="nav-item"><a class="nav-item__link js-nav-item" href="/some/place/else/else" data-page="page-name-2">OMG Page 2</a></li>
+        </ul>
+    </div>
+
+Usage
+=====
+
+    var navigationView = require('navigation')({
+      el: document.querySelector('.js-navigation')
+    });
---
+++ index.js
@@ -0,0 +1,51 @@
+var View = require('view');
+
+/**
+ * NavigationView
+ * @constructor
+ */
+var NavigationView = View.extend({
+
+  events: {
+    'click .js-nav-item': 'onClick'
+  },
+
+  elements: {
+    '.js-nav-item': 'all:items'
+  },
+
+  /**
+   * Sets the current menu item
+   * @param   {String} name
+   * @returns {NavigationView}
+   */
+  setCurrent: function(name) {
+    for (var i=0; i<this.items.length; ++i) {
+      var item = this.items[i];
+      if (item.hasAttribute('data-page')) {
+        var page = item.getAttribute('data-page');
+        if (page === name) {
+          item.classList.add('is-current');
+        } else {
+          item.classList.remove('is-current');
+        }
+      }
+    }
+    return this;
+  },
+
+  /**
+   * Handles clicks on the navigation items
+   * @param   {MouseEvent} event
+   */
+  onClick: function(event) {
+    if (event.target.hasAttribute('data-page')) {
+      this.emit('goto', event.target.getAttribute('data-page'));
+    }
+  }
+
+});
+
+module.exports = function(options) {
+  return new NavigationView(options);
+};
